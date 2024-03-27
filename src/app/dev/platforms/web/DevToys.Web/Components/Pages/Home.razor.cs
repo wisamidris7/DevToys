077b5a12457fd73b56436a2bd11083f1d8aaee7c
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-03-27 21:19:45 +0300

    khkjh

---
+++ src/app/dev/platforms/web/DevToys.Web/Components/Pages/Home.razor.cs
@@ -0,0 +1,12 @@
+﻿using DevToys.Api;
+
+namespace DevToys.Web.Components.Pages;
+
+partial class Home
+{
+
+}
+public class WSMList : List<string>, IDataGridRow<string>
+{
+    public string? Details => "WSM";
+}
